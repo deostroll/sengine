59744324bd2bafe3a28ee42a53d556bdb55c2d5e
deostroll <deostroll@gmail.com>
2016-04-27 14:26:18 +0530

    changed sequence tests: reason - change in api

--- tests/sequence_tests.py
+++ tests/sequence_tests.py
@@ -22,36 +22,36 @@ class  TesteSequence(TestCase):
 
     def test_in_sequence_horizontal(self):
         inpSeq = [ (4,6), (4, 7), (4, 8) , (4, 9)]
-        res = isValidSequence(inpSeq, 15)
+        res = isValidSequence(inpSeq)
         self.assertIsNotNone(res)
         self.assertTrue(res['result'], "not a valid sequence")
 
     def test_out_sequence_horizontal(self):
         inpSeq = [ (4,6), (4, 7), (5, 8) , (4, 9)]
-        res = isValidSequence(inpSeq, 15)
+        res = isValidSequence(inpSeq)
         self.assertIsNotNone(res)
         self.assertFalse(res['result'], "not a valid sequence")
 
     def test_in_sequence_vertical(self):
         inpSeq = [ (5, 3), (6,3), (4,3), (2, 3), (3,3)]
-        res = isValidSequence(inpSeq, 15)
+        res = isValidSequence(inpSeq)
         self.assertIsNotNone(res)
         self.assertTrue(res['result'], "not a valid sequence")
 
     def test_out_sequence_vertical(self):
         inpSeq = [ (5, 3), (6,3), (4,4), (2, 3), (3,3)]
-        res = isValidSequence(inpSeq, 15)
+        res = isValidSequence(inpSeq)
         self.assertIsNotNone(res)
         self.assertFalse(res['result'], "not a valid sequence")
 
     def test_valid_sequence_gaps_vertical(self):
         inpSeq = [ (5, 3), (6,3), (2, 3), (3,3)]
-        res = isValidSequence(inpSeq, 15)
+        res = isValidSequence(inpSeq)
         self.assertIsNotNone(res)
         self.assertTrue(res['result'], "not a valid sequence")
 
     def test_valid_sequence_gaps_horizontal(self):
         inpSeq = [ (4,6), (4, 7), (4, 9) ]
-        res = isValidSequence(inpSeq, 15)
+        res = isValidSequence(inpSeq)
         self.assertIsNotNone(res)
         self.assertTrue(res['result'], "should be a valid sequence")
